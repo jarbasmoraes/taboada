349d7c3d89a1516c74549836728c06e10ab3fd27
Jarbas de Moraes <jarbas@ae.studio>
2023-12-14 16:32:28 -0300

    Upload inicial

--- handler.js
+++ handler.js
@@ -6,7 +6,6 @@ module.exports.hello = async (event) => {
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
       },
       null,
       2
