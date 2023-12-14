155062a3c27804272481085c0a1f6ecaf5732d58
Jarbas de Moraes <jarbas@ae.studio>
2023-12-14 16:33:31 -0300

    Upload inicial

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Foi massa conhecer o Taboada em floripa!',
       },
       null,
       2
